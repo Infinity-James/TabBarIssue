0b220cc281ca7e7782045e7b4e1027ecb34d9b66
James Valaitis <james@infinityjames.com>
2025-06-20 10:54:31 +0100

    Initial Commit

---
+++ TabBarIssue/TabBarIssueApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarIssueApp.swift
+//  TabBarIssue
+//
+//  Created by James Valaitis on 20/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct TabBarIssueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TabBarIssueTests/TabBarIssueTests.swift
@@ -0,0 +1,16 @@
+//
+//  TabBarIssueTests.swift
+//  TabBarIssueTests
+//
+//  Created by James Valaitis on 20/06/2025.
+//
+
+import Testing
+
+struct TabBarIssueTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
